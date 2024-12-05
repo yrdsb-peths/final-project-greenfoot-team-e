757c4df1df8bdebe249cfd2d54f04cd6cd960ede
350444535 <150150007+350444535@users.noreply.github.com>
2024-12-04 22:29:40 -0500

    update to trader
    made him smaller by 30%

--- Trader.java
+++ Trader.java
@@ -2,6 +2,12 @@ import greenfoot.*;
 
 public class Trader extends Actor {
     public Trader() {
-        setImage("images/TraderLarger.png");
+        GreenfootImage image = new GreenfootImage("images/TraderLarger.png");
+        
+        int newWidth = (int) (image.getWidth() * 0.70);
+        int newHeight = (int) (image.getHeight() * 0.70);
+        image.scale(newWidth, newHeight);
+        
+        setImage(image);
     }
 }
