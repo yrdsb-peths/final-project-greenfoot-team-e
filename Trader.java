2506b75a1f297299cb5730716ab2af1c30db001e
350444535 <150150007+350444535@users.noreply.github.com>
2024-12-04 22:23:15 -0500

    update to tile & addition of trader & traderSpawn

---
+++ Trader.java
@@ -0,0 +1,7 @@
+import greenfoot.*; 
+
+public class Trader extends Actor {
+    public Trader() {
+        setImage("images/TraderLarger.png");
+    }
+}
--- TreasureChest.java
+++ TreasureChest.java
@@ -1,61 +1,58 @@
 import greenfoot.*; 
 import java.util.Random;
 
-public class TreasureChest extends Actor 
-{
+public class TreasureChest extends Actor {
     private static final int TILE_WIDTH = 31;  
     private static final int TILE_HEIGHT = 32;
-    private int randomNum; 
 
-    public TreasureChest()
-    {
-        randomNum = Greenfoot.getRandomNumber(100);
-        if(randomNum >= 40 && randomNum <= 60)
+    public TreasureChest() {
+        int randomNum = Greenfoot.getRandomNumber(100);
+        if (randomNum >= 40 && randomNum <= 60) 
         {
             setImage("images/ChestCoC.png");
-        }
-        else if(randomNum >= 1 && randomNum <= 11)
+        } 
+        else if (randomNum >= 1 && randomNum <= 11) 
         {
             setImage("images/ChestRaC.png");
-        }
-        else if(randomNum == 20 || randomNum == 24)
+        } 
+        else if (randomNum == 20 || randomNum == 24)
         {
             setImage("images/ChestLeC.png");
-        }
-        else
+        } 
+        else 
         {
             setImage((GreenfootImage)null);
         }
     }
 
-    public static void spawnIn(World world, String[][] grid, int xOffset, int yOffset) 
+    public static boolean spawnIn(World world, String[][] grid, int xOffset, int yOffset) 
     {
         Random random = new Random();
         int gridWidth = grid[0].length;
         int gridHeight = grid.length;
 
-        boolean chestPlaced = false;
-
-        while (!chestPlaced) 
+        if (random.nextInt(2) == 0) 
         {
-            int x = random.nextInt(gridWidth); 
-            int y = random.nextInt(gridHeight);
-
-            
-            if (!"Wall".equals(grid[y][x]))
+            for (int i = 0; i < 10; i++) 
             {
+                int x = random.nextInt(gridWidth);
+                int y = random.nextInt(gridHeight);
+
                 
-                grid[y][x] = "Chest";
+                if (!"Wall".equals(grid[y][x]) && !"Chest".equals(grid[y][x])) {
+                    grid[y][x] = "Chest"; 
 
+                    int pixelX = xOffset + x * TILE_WIDTH;
+                    int pixelY = yOffset + y * TILE_HEIGHT;
 
-                int pixelX = xOffset + x * TILE_WIDTH;
-                int pixelY = yOffset + y * TILE_HEIGHT;
-
-                world.addObject(new TreasureChest(), pixelX, pixelY);
-
-                chestPlaced = true;
+                    world.addObject(new TreasureChest(), pixelX, pixelY);
+                    return true; 
+                }
             }
         }
+
+        return false;
     }
 }
+
 
