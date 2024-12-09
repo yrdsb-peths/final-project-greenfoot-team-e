bc7c8eeb1844f9ef0ee9b146aa7a3fd8a3bab2f5
jamie wang <bombyjamie@gmail.com>
2024-12-09 12:47:48 -0500

    Update menuScreen.java

--- menuScreen.java
+++ menuScreen.java
@@ -2,8 +2,18 @@ import greenfoot.*;
 
 public class MenuScreen extends World 
 {
+    private Label titleLabel;
     public MenuScreen() {
         super(600,400,1);
+        setBackground("background.png");
+        addObject(new Button(this::goToGameScreen, "StartButtonIdle.png",114, 56), 300, 350);
+        Label titleLabel = new Label("Lurking Below", 100);
+        addObject(titleLabel, 300, 250);
     }
-    
+
+    public void goToGameScreen() 
+    {
+        //sets to the game screen when a button is pressed
+        Greenfoot.setWorld(new GameScreen()); 
+    }
 }
