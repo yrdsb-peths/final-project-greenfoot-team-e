c6838371973a9f2250d83beb9be54a911e3d1fa5
jamie wang <bombyjamie@gmail.com>
2024-12-09 13:31:10 -0500

    fd

--- menuScreen.java
+++ menuScreen.java
@@ -7,7 +7,8 @@ public class MenuScreen extends World
         super(400,600,1);
         TileWorld tileWorld = new TileWorld();
         tileWorld.generateRoomIn(this);
-        addObject(new Button(this::goToGameScreen, "StartButtonIdle.png",114, 56), 200, 350);
+        addObject(new Button(this::goToGameScreen, "load-game-button.png",114, 56), 200, 350);
+        addObject(new Button(this::goToGameScreen, "start-button.png",114, 56), 200, 250);
         Label titleLabel = new Label("Lurking Below", 40);
         addObject(titleLabel, 200, 175);
     }
