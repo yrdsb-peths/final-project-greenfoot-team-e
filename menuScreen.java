ba5a46677a21de4d295bd85cbf69a4d483d3278c
jamie wang <bombyjamie@gmail.com>
2024-12-09 12:50:33 -0500

    Update menuScreen.java

--- menuScreen.java
+++ menuScreen.java
@@ -7,8 +7,8 @@ public class MenuScreen extends World
         super(600,400,1);
         setBackground("background.png");
         addObject(new Button(this::goToGameScreen, "StartButtonIdle.png",114, 56), 300, 350);
-        Label titleLabel = new Label("Lurking Below", 100);
-        addObject(titleLabel, 300, 250);
+        Label titleLabel = new Label("Lurking Below", 60);
+        addObject(titleLabel, 300, 175);
     }
 
     public void goToGameScreen() 
