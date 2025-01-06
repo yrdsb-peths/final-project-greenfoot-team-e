588a0f85b6a2ee65686299994dcfad8a5f26ee55
350444535 <150150007+350444535@users.noreply.github.com>
2025-01-06 13:11:21 -0500

    Skeleton and Zombie Animations Code

--- Skeleton.java
+++ Skeleton.java
@@ -1,11 +1,12 @@
 import greenfoot.*;
 
 public class Skeleton extends Enemy {
-    private static final String[] IDLE_FRAMES = { "skeleton_idle_0.png", "skeleton_idle_1.png" };
-    private static final String[] WALK_FRAMES = { "skeleton_walk_0.png", "skeleton_walk_1.png" };
-    private static final String[] ATTACK_FRAMES = { "skeleton_attack_0.png", "skeleton_attack_1.png" };
-    private static final String[] HIT_FRAMES = { "skeleton_hit_0.png" };
-    private static final String[] DEATH_FRAMES = { "skeleton_death_0.png", "skeleton_death_1.png" };
+    private static final String[] IDLE_FRAMES = { "SkelIdle0.png", "SkelIdle1.png", "SkelIdle2.png", "SkelIdle3.png", "SkelIdle4.png", "SkelIdle5.png", "SkelIdle6.png", "SkelIdle7.png", "SkelIdle8.png", "SkelIdle9.png", "SkelIdle10.png" };
+    private static final String[] WALK_FRAMES = { "SkelWalk0.png", "SkelWalk1.png", "SkelWalk2.png", "SkelWalk3.png", "SkelWalk4.png", "SkelWalk5.png", "SkelWalk6.png", "SkelWalk7.png", "SkelWalk8.png", "SkelWalk9.png", "SkelWalk10.png", "SkelWalk11.png", "SkelWalk12.png" };
+    private static final String[] ATTACK_FRAMES = { "SkelAttack0.png", "SkelAttack1.png", "SkelAttack2.png", "SkelAttack3.png", "SkelAttack4.png", "SkelAttack5.png", "SkelAttack6.png", "SkelAttack7.png", "SkelAttack8.png", "SkelAttack9.png", "SkelAttack10.png", "SkelAttack11.png", "SkelAttack12.png", "SkelAttack13.png", "SkelAttack14.png", "SkelAttack15.png", "SkelAttack16.png", "SkelAttack17.png" };
+    private static final String[] HIT_FRAMES = { "SkelHit0.png", "SkelHit1.png", "SkelHit2.png", "SkelHit3.png", "SkelHit4.png", "SkelHit5.png","SkelHit6.png", "SkelHit7.png", "SkelHit8.png" };
+    private static final String[] DEATH_FRAMES = { "SkelDeath0.png", "SkelDeath1.png", "SkelDeath2.png", "SkelDeath3.png", "SkelDeath4.png", "SkelDeath5.png", "SkelDeath6.png", "SkelDeath7.png", "SkelDeath8.png", "SkelDeath9.png", "SkelDeath10.png", "SkelDeath11.png", "SkelDeath12.png", "SkelDeath13.png", "SkelDeath14.png" };
+    private static final String[] REACT_FRAMES = { "SkelReact0.png", "SkelReact1.png", "SkelReact2.png", "SkelReact3.png"};
 
     public Skeleton() {
         changeState(State.IDLE);
@@ -34,5 +35,9 @@ public class Skeleton extends Enemy {
     @Override
     protected String[] getDeathFrames() {
         return DEATH_FRAMES;
+    }
+
+    protected String[] getReactFrames() {
+        return REACT_FRAMES;
     }
 }
--- Zombie.java
+++ Zombie.java
@@ -1,11 +1,12 @@
 import greenfoot.*;
 
 public class Zombie extends Enemy {
-    private static final String[] IDLE_FRAMES = { "zombie_idle_0.png", "zombie_idle_1.png" };
-    private static final String[] WALK_FRAMES = { "zombie_walk_0.png", "zombie_walk_1.png" };
-    private static final String[] ATTACK_FRAMES = { "zombie_attack_0.png", "zombie_attack_1.png" };
-    private static final String[] HIT_FRAMES = { "zombie_hit_0.png" };
-    private static final String[] DEATH_FRAMES = { "zombie_death_0.png", "zombie_death_1.png" };
+    private static final String[] IDLE_FRAMES = { "ZombieIdle0.png", "ZombieIdle1.png", "ZombieIdle2.png", "ZombieIdle3.png", "ZombieIdle4.png", "ZombieIdle5.png", "ZombieIdle6.png", "ZombieIdle7.png"};
+    private static final String[] WALK_FRAMES = { "ZombieWalk0.png", "ZombieWalk1.png", "ZombieWalk2.png", "ZombieWalk3.png", "ZombieWalk4.png", "ZombieWalk5.png", "ZombieWalk6.png", "ZombieWalk7.png" };
+    private static final String[] ATTACK_FRAMES = { "ZombieAttack0.png", "ZombieAttack1.png", "ZombieAttack2.png", "ZombieAttack3.png", "ZombieAttack4.png", "ZombieAttack5.png", "ZombieAttack6.png" };
+    private static final String[] HIT_FRAMES = { "ZombieHit0.png", "ZombieHit1.png", "ZombieHit2.png", "ZombieHit3.png" };
+    private static final String[] DEATH_FRAMES = { "ZombieDeath0.png", "ZombieDeath1.png", "ZombieDeath2.png", "ZombieDeath3.png", "ZombieDeath4.png" , "ZombieDeath5.png", "ZombieDeath6.png", "ZombieDeath7.png"};
+    private static final String[] HEAVY_ATTACK_FRAMES = { "ZombieHeavyAttack0.png", "ZombieHeavyAttack1.png", "ZombieHeavyAttack2.png", "ZombieHeavyAttack3.png", "ZombieHeavyAttack4.png", "ZombieHeavyAttack5.png", "ZombieHeavyAttack6.png", "ZombieHeavyAttack7.png", "ZombieHeavyAttack8.png", "ZombieHeavyAttack9.png", "ZombieHeavyAttack10.png", "ZombieHeavyAttack11.png", "ZombieHeavyAttack12.png", "ZombieHeavyAttack13.png", "ZombieHeavyAttack14.png", "ZombieHeavyAttack15.png", "ZombieHeavyAttack16.png", "ZombieHeavyAttack17.png", "ZombieHeavyAttack18.png", "ZombieHeavyAttack19.png", "ZombieHeavyAttack20.png", "ZombieHeavyAttack21.png"};
 
     public Zombie() {
         changeState(State.IDLE);
@@ -34,5 +35,9 @@ public class Zombie extends Enemy {
     @Override
     protected String[] getDeathFrames() {
         return DEATH_FRAMES;
+    }
+
+    protected String[] getHeavyAttackFrames() {
+        return HEAVY_ATTACK_FRAMES;
     }
 }
