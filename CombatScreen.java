656d48615ae03e98400f735bb539abe66e029cf4
350444535 <150150007+350444535@users.noreply.github.com>
2025-01-07 13:16:29 -0500

    wiped old placeholder for comabtscreen

--- CombatScreen.java
+++ CombatScreen.java
@@ -1,11 +1,8 @@
 import greenfoot.*;
 public class CombatScreen extends World {
-    public CombatScreen(){
-    super(400,600,1);
-    addObject(new Button(this::returnGameScreen, "StartButtonIdle.png",114, 56), 200, 350);
-    }
-    public void returnGameScreen(){
-        Greenfoot.setWorld(new GameScreen());
+    public CombatScreen()
+    {
+        super(400,600,1);
     }
 }
 
