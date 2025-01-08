8b88d1a29b842ab10fdb87ce4c6f742bd30fb5c2
350444535 <150150007+350444535@users.noreply.github.com>
2025-01-08 12:24:22 -0500

    Update CombatScreen.java

--- CombatScreen.java
+++ CombatScreen.java
@@ -1,8 +1,32 @@
 import greenfoot.*;
-public class CombatScreen extends World {
-    public CombatScreen()
-    {
-        super(400,600,1);
+public class CombatScreen extends World 
+{
+    private static final int TILE_WIDTH = 31;
+    private static final int TILE_HEIGHT = 32;
+    public static final int ROOM_WIDTH = 400;
+    public static final int ROOM_HEIGHT = 400;
+
+    private int gridWidth;
+    private int gridHeight;
+    private String[][] grid;
+
+    public CombatScreen(Object world)
+        {
+            super(400,600,1);
+            
+            generateCombatRoom(world);
+    }
+        
+    private void generateCombatRoom(World world) 
+        {
+            gridWidth = ROOM_WIDTH / TILE_WIDTH;
+            gridHeight = ROOM_HEIGHT / TILE_HEIGHT;
+            grid = new String[gridHeight][gridWidth];
+            
+            int worldWidth = world.getWidth();
+        int worldHeight = world.getHeight();
+        int xOffset = (worldWidth - ROOM_WIDTH) / 2 + 30;
+        int yOffset = ((worldHeight - ROOM_HEIGHT) / 2) - 80;
     }
 }
 
