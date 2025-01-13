6dddb02566e3a62db6cc8537a5d923711a006cd3
350444535 <150150007+350444535@users.noreply.github.com>
2025-01-13 12:32:50 -0500

    v1 p5

--- CombatScreen.java
+++ CombatScreen.java
@@ -1,6 +1,7 @@
 import greenfoot.*;
 
-public class CombatScreen extends World {
+public class CombatScreen extends World 
+{
 
     public CombatScreen()
     {
@@ -58,6 +59,82 @@ public class CombatScreen extends World {
         addObject(floor52, 154,212);
         WallI wall52 = new WallI();
         addObject(wall52 , 154, 212);
+        FloorI floor13 = new FloorI();
+        addObject(floor13, 185,84);
+        WallI wall13 = new WallI();
+        addObject(wall13, 185, 84);
+        FloorII floor23 = new FloorII();
+        addObject(floor23, 185,116);
+        FloorII floor33 = new FloorII();
+        addObject(floor33, 185,148);
+        FloorII floor43 = new FloorII();
+        addObject(floor43, 185,180);
+        FloorI floor53 = new FloorI();
+        addObject(floor53, 185,212);
+        WallII wall53 = new WallII();
+        addObject(wall53 , 185, 212);
+        FloorII floor14 = new FloorII();
+        addObject(floor14, 216,84);
+        WallII wall14 = new WallII();
+        addObject(wall14, 216, 84);
+        FloorI floor24 = new FloorI();
+        addObject(floor24, 216,116);
+        FloorI floor34 = new FloorI();
+        addObject(floor34, 216,148);
+        FloorII floor44 = new FloorII();
+        addObject(floor44, 216,180);
+        FloorII floor54 = new FloorII();
+        addObject(floor54, 154,212);
+        WallI wall54 = new WallI();
+        addObject(wall54 , 216, 212);
+        FloorII floor15 = new FloorII();
+        addObject(floor15, 247,84);
+        WallI wall15 = new WallI();
+        addObject(wall15, 247, 84);
+        FloorII floor25 = new FloorII();
+        addObject(floor25, 247,116);
+        FloorI floor35 = new FloorI();
+        addObject(floor35, 247,148);
+        FloorI floor45 = new FloorI();
+        addObject(floor45, 247,180);
+        FloorI floor55 = new FloorI();
+        addObject(floor55, 247,212);
+        WallII wall55 = new WallII();
+        addObject(wall55 , 247, 212);
+        FloorI floor16 = new FloorI();
+        addObject(floor16, 278,84);
+        WallI wall16 = new WallI();
+        addObject(wall16, 278, 84);
+        FloorII floor26 = new FloorII();
+        addObject(floor26, 278,116);
+        FloorI floor36 = new FloorI();
+        addObject(floor36, 278,148);
+        FloorII floor46 = new FloorII();
+        addObject(floor46, 278,180);
+        FloorII floor56 = new FloorII();
+        addObject(floor56, 278,212);
+        WallII wall56 = new WallII();
+        addObject(wall56 , 278, 212);
+        FloorII floor17 = new FloorII();
+        addObject(floor17, 92, 84);
+        CornerTR cornertr = new CornerTR();
+        addObject(cornertr, 92, 84);
+        FloorI floor27 = new FloorI();
+        addObject(floor27, 92, 116);
+        RightWall rightwall1 = new RightWall();
+        addObject(rightwall1, 92, 116);
+        FloorII floor37 = new FloorII();
+        addObject(floor37, 92, 148);
+        RightWall rightwall2 = new RightWall();
+        addObject(rightwall2, 92, 148);
+        FloorI floor47 = new FloorI();
+        addObject(floor47, 92, 180);
+        RightWall rightwall3 = new RightWall();
+        addObject(rightwall3, 92, 180);
+        FloorI floor57 = new FloorI();
+        addObject(floor57, 92, 212);
+        RightWall rightwall4 = new RightWall();
+        addObject(rightwall4, 92, 212);
     }
 
 }
