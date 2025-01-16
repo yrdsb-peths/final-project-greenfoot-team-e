7c00a5d7db9145b9f8c8b51bdcc7207716ddb4f5
jamie wang <bombyjamie@gmail.com>
2025-01-15 21:38:21 -0500

    Create WinScreen.java

---
+++ WinScreen.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import greenfoot.*;
+
+public class WinScreen extends World {
+    
+}
