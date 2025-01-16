c2d7c9809dc89277c7a79037bc2c46c07692193d
jamie wang <bombyjamie@gmail.com>
2025-01-15 21:43:24 -0500

    created win screen

--- WinScreen.java
+++ WinScreen.java
@@ -1,11 +1,17 @@
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
-import greenfoot.*;
+import greenfoot.Greenfoot;
+import greenfoot.GreenfootImage;
+import greenfoot.World;
 
 public class WinScreen extends World {
-    
+    public WinScreen(){    
+        super(400, 600, 1);
+        setBackground()
+    }
+
 }
